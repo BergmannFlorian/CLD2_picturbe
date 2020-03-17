45660bb390ff02d0e7b490b028389e341a2174b9
Florian Bergmann <flobergmann@aol.ch>
2020-03-17 11:54:39 +0100

    update deploy task

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,32 @@
-
-
-set :deploy_to, "/home/cld2_8/cld2-8.mycpnv.ch"
+# Production server access
 server "cld2-8.mycpnv.ch", user: "cld2_8"
 
+set :deploy_to, "/home/cld2_8/cld2-8.mycpnv.ch"
+
+set :keep_releases, 3
 set :ssh_options, {
     keys: %w(config/ssh/swisscenter_cld2_8_rsa),
     forward_agent: false,
     auth_methods: %w(publickey)
 }
+
+# Add package
+SSHKit.config.command_map[:composer] = "php -d allow_url_fopen=true #{shared_path.join('composer')}"
+
+# Ignore
+Rake::Task['laravel:optimize'].clear_actions rescue nil
+set :laravel_set_acl_paths, false
+set :laravel_upload_dotenv_file_on_deploy, false
+
+# Task
+after 'composer:run', 'copy_dotenv'
+task :copy_dotenv do
+    on roles(:all) do
+        execute :cp, "#{shared_path}/.env #{release_path}/.env"
+    end
+end
+
+after 'composer:run', 'laravel:migrate'
 
 # server-based syntax
 # ======================
